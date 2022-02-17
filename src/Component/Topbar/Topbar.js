fc775e0afaa6a098425a9fd077ec99b6ddca98e8
Nadim Sheikh <nadim.sheikh.07@gmail.com>
2022-02-17 11:13:55 +0530

    load dynamic component

---
+++ src/Component/Topbar/Topbar.js
@@ -0,0 +1,10 @@
+import "./Topbar.scss"
+const Topbar = () => {
+    return (
+        <div className="topbar">
+            <h1>Topbar</h1>
+        </div>
+    )
+}
+
+export default Topbar;
--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,17 @@
+import React, { Suspense } from 'react';
+const component_url = "Component/Topbar/Topbar"
 function Home(props) {
-  return <h1 className="red">Hello {props.name}!</h1>;
+
+  const Topbar = React.lazy(() => import(`./${component_url}`));
+
+  return (
+    <div className="Home">
+      <Suspense fallback={<div>Loading...</div>}>
+        <Topbar />
+      </Suspense>
+      <h1 className="red">Hello {props.name}!</h1>
+    </div>
+  );
 };
 
 export default Home;
